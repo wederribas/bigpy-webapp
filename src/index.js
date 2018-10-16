a9de3e00cf4bfbf8022329995eead85e515be0cd
Weder Ribas <wederribas@gmail.com>
2018-10-15 22:13:12 -0300

    Add GraphQL Apollo client to main app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './components/App/App'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import registerServiceWorker from './registerServiceWorker'
+import App from './components/App/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8000/graphql/'
+})
+
+export const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
