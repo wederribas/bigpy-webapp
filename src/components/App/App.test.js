703634aeae17ba2c43d41b7eec29bc3e2267baad
Weder Ribas <wederribas@gmail.com>
2018-10-15 22:18:29 -0300

    Mock Apollo GraphQL provider in tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { MockedProvider } from 'react-apollo/test-utils'
+import { render } from 'react-testing-library'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+test('renders without crashing', () => {
+  const { container } = render(
+    <MockedProvider addTypename={false}>
+      <App />
+    </MockedProvider>
+  )
+
+  expect(container.firstChild).toMatchSnapshot()
+})
