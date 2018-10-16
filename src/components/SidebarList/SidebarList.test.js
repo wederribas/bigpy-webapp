4133d64356e03cc5c7c898905f4558abac718030
Weder Ribas <wederribas@gmail.com>
2018-10-15 22:26:57 -0300

    Wrap sidebar test renderer with MemoryRouter to fix router context

--- src/components/SidebarList/SidebarList.test.js
+++ src/components/SidebarList/SidebarList.test.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
 import { render } from '../../utils/test-utils'
 import SidebarList from './SidebarList'
 
 test('Sidebar list', () => {
-  const { getByText } = render(<SidebarList />)
+  const { getByText, container } = render(
+    <MemoryRouter>
+      <SidebarList />
+    </MemoryRouter>
+  )
 
   expect(getByText('Search')).toHaveTextContent('Search')
   expect(getByText('Top 10')).toHaveTextContent('Top 10')
   expect(getByText('Insights')).toHaveTextContent('Insights')
   expect(getByText('About')).toHaveTextContent('About')
+  expect(container.firstChild).toMatchSnapshot()
 })
