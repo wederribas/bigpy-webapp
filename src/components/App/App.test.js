0323ee8d575d4f25015ae5241d62e019be66d6cd
wederribas <wederribas@gmail.com>
2018-10-21 23:45:37 -0300

    Add Graphql mocks to include Search.js component query

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { MockedProvider } from 'react-apollo/test-utils'
 import { render } from 'react-testing-library'
+import { mocks } from '../../utils/graphql-test-mocks'
 import App from './App'
 
 test('renders without crashing', () => {
   const { container } = render(
-    <MockedProvider addTypename={false}>
+    <MockedProvider mocks={mocks} addTypename={false}>
       <App />
     </MockedProvider>
   )
