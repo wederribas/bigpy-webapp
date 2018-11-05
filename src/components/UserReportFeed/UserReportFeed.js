c8382ca641656bbcbc2573ffe607e34951d1bafd
wederribas <wederribas@gmail.com>
2018-11-05 21:57:19 -0200

    Fix feed list extra side / Add loader when button is clicked

--- src/components/UserReportFeed/UserReportFeed.js
+++ src/components/UserReportFeed/UserReportFeed.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
@@ -10,9 +10,11 @@ import {
   List,
   ListItem,
   ListItemText,
-  Divider
+  Divider,
+  Grid
 } from '@material-ui/core'
 import ErrorMessage from '../ErrorMessage/ErrorMessage'
+import BouncingLoader from '../BouncingLoader/BouncingLoader'
 
 const styles = theme => ({
   root: {
@@ -25,6 +27,15 @@ const styles = theme => ({
   },
   feedListItem: {
     display: 'block'
+  },
+  loader: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: theme.spacing.unit * 10
+  },
+  loadButton: {
+    display: 'flex',
+    justifyContent: 'center'
   }
 })
 
@@ -51,17 +62,30 @@ export const USER_REPORT_FEED_QUERY = gql`
   }
 `
 
+function Loading(props) {
+  return (
+    <div className={props.classes.loader}>
+      <BouncingLoader />
+    </div>
+  )
+}
+
+Loading.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
 function LoadMoreButton(props) {
   const { classes } = props
   return (
-    <Button
-      variant="contained"
-      color="primary"
-      className={classes.button}
-      onClick={() => props.loadMore()}
-    >
-      Load More
-    </Button>
+    <div className={classes.loadButton}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => props.loadMore()}
+      >
+        Load More
+      </Button>
+    </div>
   )
 }
 
@@ -74,92 +98,114 @@ function ReportsFeed(props) {
   const { classes } = props
   const totalReports = props.reports.length
   return (
-    <Paper elevation={1} className={`${classes.root} ${props.classes.feed}`}>
-      <Typography variant="h4" component="h2">
-        Users Reports
-      </Typography>
-      <List data-testid="reports-list">
-        {props.reports.map((report, index) => {
-          const date = new Date(report.node.date)
-          return (
-            <div key={report.node.Id}>
-              <ListItem className={classes.feedListItem}>
-                <ListItemText
-                  primary={`${report.node.city} - ${report.node.state}`}
-                  secondary={date.toDateString()}
-                />
-                <Typography component="p">{report.node.userReport}</Typography>
-                <Typography component="p">
-                  {report.node.userFeedback}
-                </Typography>
-                <Typography component="p">{report.node.userRating}</Typography>
-              </ListItem>
-              {totalReports === index + 1 || <Divider />}
-            </div>
-          )
-        })}
-      </List>
-      <LoadMoreButton loadMore={props.loadMore} classes={props.classes} />
-    </Paper>
+    <Grid>
+      <Paper elevation={1} className={`${classes.root} ${props.classes.feed}`}>
+        <Typography variant="h4" component="h2">
+          Users Reports
+        </Typography>
+        <List data-testid="reports-list">
+          {props.reports.map((report, index) => {
+            const date = new Date(report.node.date)
+            return (
+              <div key={report.node.Id}>
+                <ListItem className={classes.feedListItem}>
+                  <ListItemText
+                    primary={`${report.node.city} - ${report.node.state}`}
+                    secondary={date.toDateString()}
+                  />
+                  <Typography component="p">
+                    {report.node.userReport}
+                  </Typography>
+                  <Typography component="p">
+                    {report.node.userFeedback}
+                  </Typography>
+                  <Typography component="p">
+                    {report.node.userRating}
+                  </Typography>
+                </ListItem>
+                {totalReports === index + 1 || <Divider />}
+              </div>
+            )
+          })}
+        </List>
+        {props.isLoadingMore ? (
+          <BouncingLoader />
+        ) : (
+          <LoadMoreButton loadMore={props.loadMore} classes={props.classes} />
+        )}
+      </Paper>
+    </Grid>
   )
 }
 
 ReportsFeed.propTypes = {
   classes: PropTypes.object.isRequired,
   reports: PropTypes.array.isRequired,
-  loadMore: PropTypes.func.isRequired
+  loadMore: PropTypes.func.isRequired,
+  isLoadingMore: PropTypes.bool.isRequired
 }
 
-function UserReportFeed(props) {
-  return (
-    <Query
-      query={USER_REPORT_FEED_QUERY}
-      variables={{ companyName: props.companyName }}
-    >
-      {({ loading, error, data, fetchMore }) => {
-        if (loading) return null
+class UserReportFeed extends Component {
+  state = {
+    isLoadingMore: false
+  }
 
-        if (error) return <ErrorMessage />
+  render() {
+    return (
+      <Query
+        query={USER_REPORT_FEED_QUERY}
+        variables={{ companyName: this.props.companyName }}
+      >
+        {({ loading, error, data, fetchMore }) => {
+          if (loading) return <Loading classes={this.props.classes} />
+          if (error) return <ErrorMessage />
 
-        const paginationCursor = data.allCustomerReports.pageInfo.endCursor
-        const reports = data.allCustomerReports.edges
+          const paginationCursor = data.allCustomerReports.pageInfo.endCursor
+          const reports = data.allCustomerReports.edges
 
-        const loadMore = () =>
-          fetchMore({
-            variables: {
-              cursor: paginationCursor
-            },
-            updateQuery: (previousResult, { fetchMoreResult }) => {
-              const newEdges = fetchMoreResult.allCustomerReports.edges
-              const pageInfo = fetchMoreResult.allCustomerReports.pageInfo
+          const loadMore = () => {
+            this.setState({ isLoadingMore: true })
+            return fetchMore({
+              variables: {
+                cursor: paginationCursor
+              },
+              updateQuery: (previousResult, { fetchMoreResult }) => {
+                const newEdges = fetchMoreResult.allCustomerReports.edges
+                const pageInfo = fetchMoreResult.allCustomerReports.pageInfo
 
-              return newEdges.length
-                ? {
-                    allCustomerReports: {
-                      __typename: previousResult.allCustomerReports.__typename,
-                      edges: [
-                        ...previousResult.allCustomerReports.edges,
-                        ...newEdges
-                      ],
-                      pageInfo
+                this.setState({ isLoadingMore: false })
+
+                return newEdges.length
+                  ? {
+                      allCustomerReports: {
+                        __typename:
+                          previousResult.allCustomerReports.__typename,
+                        edges: [
+                          ...previousResult.allCustomerReports.edges,
+                          ...newEdges
+                        ],
+                        pageInfo
+                      }
                     }
-                  }
-                : previousResult
-            }
-          })
+                  : previousResult
+              }
+            })
+          }
 
-        return (
-          <div data-testid="feed">
-            <ReportsFeed
-              reports={reports}
-              classes={props.classes}
-              loadMore={loadMore}
-            />
-          </div>
-        )
-      }}
-    </Query>
-  )
+          return (
+            <div data-testid="feed">
+              <ReportsFeed
+                reports={reports}
+                classes={this.props.classes}
+                loadMore={loadMore}
+                isLoadingMore={this.state.isLoadingMore}
+              />
+            </div>
+          )
+        }}
+      </Query>
+    )
+  }
 }
 
 UserReportFeed.propTypes = {
