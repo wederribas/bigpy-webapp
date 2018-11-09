1cca0ca012ca246bef1b0855322a9602216eebac
wederribas <wederribas@gmail.com>
2018-11-08 22:40:21 -0200

    Make sidebar menu responsive for mobile devices

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import Drawer from '@material-ui/core/Drawer'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import { IconButton } from '@material-ui/core'
+import { IconButton, Hidden } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
 import SidebarList from '../SidebarList/SidebarList'
 import GitHubIcon from '../Icons/GitHubIcon'
+
+const drawerWidth = 200
 
 const styles = theme => ({
   root: {
@@ -21,44 +24,98 @@ const styles = theme => ({
   },
   drawerPaper: {
     position: 'relative',
-    width: 200
+    width: 200,
+    height: '100vh'
+  },
+  drawer: {
+    marginTop: 65,
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  menuButton: {
+    marginRight: 20,
+    [theme.breakpoints.up('sm')]: {
+      display: 'none'
+    }
   },
   toolbar: theme.mixins.toolbar
 })
 
-function Navigation(props) {
-  const { classes } = props
-  return (
-    <div className={classes.root}>
-      <AppBar position="absolute" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            BigPy — Customer Data Analysis
-          </Typography>
-          <div className={classes.grow} />
-          <IconButton
-            color="inherit"
-            href="https://github.com/wederribas/bigpy-webapp"
-          >
-            <GitHubIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper
-        }}
-      >
-        <div className={classes.toolbar} />
-        <SidebarList />
-      </Drawer>
-    </div>
-  )
+class Navigation extends Component {
+  state = {
+    mobileOpen: false
+  }
+
+  handleDrawerToggle = () => {
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }))
+  }
+
+  render() {
+    const { classes, theme } = this.props
+    return (
+      <div className={classes.root}>
+        <AppBar position="absolute" className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={this.handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" noWrap>
+              BigPy — Customer Data Analysis
+            </Typography>
+            <div className={classes.grow} />
+            <IconButton
+              color="inherit"
+              href="https://github.com/wederribas/bigpy-webapp"
+            >
+              <GitHubIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <nav className={classes.drawer}>
+          <Hidden smUp implementation="css">
+            <Drawer
+              //container={this.props.container}
+              variant="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={this.state.mobileOpen}
+              onClose={this.handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper
+              }}
+              ModalProps={{
+                keepMounted: true
+              }}
+            >
+              <SidebarList />
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Drawer
+              classes={{
+                paper: classes.drawerPaper
+              }}
+              variant="permanent"
+              open
+            >
+              <SidebarList />
+            </Drawer>
+          </Hidden>
+        </nav>
+      </div>
+    )
+  }
 }
 
 Navigation.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(Navigation)
+export default withStyles(styles, { withTheme: true })(Navigation)
